369ca71130011b077bd489db37f65880618c4363
Prathameshk11 <prathameshkhokaralkar@gmail.com>
2024-01-19 17:59:21 +0530

    Brainiac and PromptSaga and Team_Registraion

--- pages/Braniac.py
+++ pages/Braniac.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
-st.title('Team Registration for Treasure Trove')
+st.title('Time-Warped Brainiac: Teams, Rewind Your Maps!')
+
+
+def display_brainiac_event():
+    st.markdown("## Brainiac Challenge Solo Participants:")
+    st.markdown("🚀 Greetings Time-Traveling Brainiac! 🧠⏳")
+    st.markdown("Whoa! You've bravely embarked on the Brainiac Challenge solo journey. "
+                "No need for a sidekick when you're the superhero of intellect! 🦸‍♂️💡\n\n"
+                "Prepare for mind-bending aptitude tests, intellectual debates, and a mock interview "
+                "(don't worry, the interviewers are nice time-traveling folks). "
+                "Your solo mission: to conquer brains across the space-time continuum! 🌌✨\n\n"
+                "Solo maestro, your wit is the DeLorean, and you're about to hit 88 mph into "
+                "the solo adventure of a lifetime!\n\n"
+                "P.S. Remember, in the time-traveling circus of brilliance, you're the solo acrobat! 🎩🌟")
+
+
--- pages/PromptSaga.py
+++ pages/PromptSaga.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
-st.title('Team Registration for Treasure Trove')
+st.title('Timeless Solo Story: Teams, Exit Time Tunnel!')
+
+def display_prompt_saga_event():
+    st.markdown("## Prompt Saga Solo Participants:")
+    st.markdown("🎭 Greetings Time-Traveling Storyteller! 📜⏳")
+    st.markdown("A round of applause! You're the star of Prompt Saga's solo show. "
+                "No need for a supporting cast – your creativity steals the spotlight! 🌟🚀\n\n"
+                "Craft a visual saga with AI-generated images and embark on a storytelling escapade "
+                "that makes history (and future history). Your solo storytelling prowess is like "
+                "a stand-up routine for time-traveling epochs! 🤹‍♀️📽️\n\n"
+                "Solo storyteller, your creativity is a cosmic stand-up routine. "
+                "Get ready to drop some punchlines in the time vortex!\n\n"
+                "P.S. In the grand theater of creativity, you're the solo superstar! 🎤🌌")
