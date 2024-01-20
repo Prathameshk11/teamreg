ba44c13e7e7b633aae12364e0dee82bdc4e8847f
Prathameshk11 <prathameshkhokaralkar@gmail.com>
2024-01-20 16:45:32 +0530

    Team_Registraion

--- Team_Registraion.py
+++ Team_Registraion.py
@@ -20,12 +20,12 @@ page_by_img = '''
 <style>
 [data-testid="stApp"]::before {
     content: '';
-    position: fixed;
+    position: absolute;
     top: 0;
     left: 0;
     width: 100vw;
     height: 100vh;
-    background: url("https://th.bing.com/th/id/OIP.kB4N8I2x9xGnrIZneU7q2QHaNJ?pid=ImgDet&w=188&h=333&c=7&dpr=1.5") center center / cover;
+    background: url("https://i.ibb.co/LdT0BKn/hourglass-with-sand-middle-word-sand-it.jpg") center center / cover;
     opacity: 0.5; /* Adjust the overlay opacity value as needed (0.0 to 1.0) */
     
 }
