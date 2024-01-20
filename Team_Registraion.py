2e11f515e74c03ec57f33e18a4506ffa039afc04
Prathameshk11 <prathameshkhokaralkar@gmail.com>
2024-01-20 17:37:36 +0530

    Team_Registraion

--- Team_Registraion.py
+++ Team_Registraion.py
@@ -25,19 +25,19 @@ page_by_img = '''
     left: 0;
     width: 100vw;
     height: 100vh;
-    background: url("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/aaf421ab-32a1-4f50-a205-3cf74d85063d/dfwcif4-045c2496-191e-4d7b-be5e-489e31d35dd5.png/v1/fill/w_1280,h_725,q_80,strp/time_traveling_machines__1_by_veesyrsfantasy_ai_dfwcif4-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2FhZjQyMWFiLTMyYTEtNGY1MC1hMjA1LTNjZjc0ZDg1MDYzZFwvZGZ3Y2lmNC0wNDVjMjQ5Ni0xOTFlLTRkN2ItYmU1ZS00ODllMzFkMzVkZDUucG5nIiwiaGVpZ2h0IjoiPD03MjUiLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS53YXRlcm1hcmsiXSwid21rIjp7InBhdGgiOiJcL3dtXC9hYWY0MjFhYi0zMmExLTRmNTAtYTIwNS0zY2Y3NGQ4NTA2M2RcL3ZlZXN5cnNmYW50YXN5LWFpLTQucG5nIiwib3BhY2l0eSI6OTUsInByb3BvcnRpb25zIjowLjQ1LCJncmF2aXR5IjoiY2VudGVyIn19.EhbE0PxnYj3QTPIubIuNRCH5vE1QqESpwB6nBu41nQ4") center center / cover;
+    background: url("https://l1nq.com/qF7MD") center center / cover;
     opacity: 0.5; /* Adjust the overlay opacity value as needed (0.0 to 1.0) */
 }
 [data-testid="stHeader"] {
 background: rgba(0,0,0,0);
 }
-
+[class="st-emotion-cache-10trblm e1nzilvr1"]
 </style>
 '''
 st.markdown(page_by_img, unsafe_allow_html=True)
 
 
-image_url = "https://i.ibb.co/GVR0QvY/acunetixheader.png"
+image_url = "https://i.ibb.co/1dCdYrG/Acunetix-gform-header.png"
 st.image(image_url, use_column_width=True) 
 
 # Function to write data to Google Sheet
